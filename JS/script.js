40b599781855c2f988c9cf57feaa8c2fc23c57ca
dcreta <83987958+dcreta@users.noreply.github.com>
2023-06-12 10:56:49 -0300

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,43 +1,35 @@
-let stock = 0;
-const producto = "Producto A";
+let stockA = 0;
+let stockB = 10;
+let stockC = 5;
 
 function cargarStock() {
-    const cantidad = parseInt(prompt(`Ingrese la cantidad de unidades de ${producto} a cargar:`));
+    const cantidad = parseInt(document.getElementById("cantidad").value);
     if (isNaN(cantidad)) {
         console.log("La cantidad ingresada no es válida.");
         return;
     }
-  
+
     for (let i = 1; i <= cantidad; i++) {
-        if (stock < 30) {
-            stock++;
-            console.log(`Se ha cargado una unidad de ${producto}. Stock actual: ${stock}`);
+        if (stockA < 30) {
+            stockA++;
+            console.log(`Se ha cargado una unidad de Producto A. Stock actual: ${stockA}`);
         } else {
-            console.log(`No se puede cargar más stock de ${producto}. Stock máximo alcanzado.`);
+            console.log(`No se puede cargar más stock de Producto A. Stock máximo alcanzado.`);
             break;
         }
     }
+
+    actualizarStock();
 }
 
-const precioUnitario = 10.99;
-const proveedor = "Proveedor X";
-
-const productoObjeto = {
-  nombre: producto,
-  stockInicial: stock,
-  stockActual: stock,
-  precioUnitario: precioUnitario,
-  proveedor: proveedor
-};
-
-const productos = [
-  productoObjeto,
-  { nombre: "Producto B", stockInicial: 10, stockActual: 10, precioUnitario: 15.99, proveedor: "Proveedor Y" },
-  { nombre: "Producto C", stockInicial: 5, stockActual: 5, precioUnitario: 8.99, proveedor: "Proveedor Z" }
-];
-
-function buscarProducto(nombre) {
-  return productos.filter(producto => producto.nombre.toLowerCase().includes(nombre.toLowerCase()));
+function actualizarStock() {
+    document.getElementById("stock").textContent = stockA + stockB + stockC;
+    document.getElementById("stockInicialA").textContent = stockA;
+    document.getElementById("stockActualA").textContent = stockA;
+    document.getElementById("stockInicialB").textContent = stockB;
+    document.getElementById("stockActualB").textContent = stockB;
+    document.getElementById("stockInicialC").textContent = stockC;
+    document.getElementById("stockActualC").textContent = stockC;
 }
 
-cargarStock();
+actualizarStock();
